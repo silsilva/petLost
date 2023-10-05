f6469cb1fad20e5b446e0bdbcf8e563e3499ca4b
Sil <79480678+silsilva@users.noreply.github.com>
2023-10-05 18:12:13 -0300

    Update auth-controller.ts

--- server/controllers/auth-controller.ts
+++ server/controllers/auth-controller.ts
@@ -96,3 +96,29 @@ export async function getToken(UserData: UserData) {
     return false;
   }
 }
+export async function createAuth(userId, email, password) {
+  const [auth, authCreated] = await Auth.findOrCreate({
+    where: { user_id: userId },
+    defaults: {
+      email: email,
+      password: password,
+      user_id: userId,
+    },
+  });
+
+  return {
+    auth,
+    authCreated,
+  };
+}
+export async function authId(email, password) {
+  try {
+    const res = await Auth.findOne({
+      where: { email: email, password: password },
+    });
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
